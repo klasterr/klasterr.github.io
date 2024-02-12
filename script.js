eee381cfbb622257e758abd7a58865756731027b
klasterr <kuzjovictor@gmail.com>
2024-02-12 23:22:18 +0200

    Initial8

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("Nobutton").addEventListener("click", () => {
+    document.getElementById("Nobutton").style.fontSize = '16px'
+    document.getElementById("Nobutton").style.padding = '0 10px'
+})
